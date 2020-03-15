e0bdd2f22755ba97c74db493adaef56ac9600c67
Cody Engel <cengel815@gmail.com>
2020-03-15 12:54:58 -0500

    Hello CI
    * Added CircleCI to the project with an initial workflow to get started.
    * Added Detekt to the project with an incredibly strict ruleset.
    * Added JUnit 5, MockK, and Strikt to help with writing unit tests.
    * Added and configured Dokka for generating kdoc.
    * Started to add documentation for fakek and FakeK.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.3.70"
+    id("io.gitlab.arturbosch.detekt").version("1.6.0")
+    id("org.jetbrains.dokka").version("0.10.1")
 }
 
 group = "dev.engel.fakek"
 version = "0.1-SNAPSHOT"
 
 repositories {
+    jcenter()
     mavenCentral()
 }
 
@@ -15,7 +18,9 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.github.javafaker:javafaker:1.0.2")
 
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.0.0-BETA2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
+    testImplementation("io.mockk:mockk:1.9")
+    testImplementation("io.strikt:strikt-core:0.24.0")
 }
 
 tasks.withType<KotlinCompile> {
@@ -24,4 +29,34 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTask> {
+    outputFormat = "html"
+    outputDirectory = "build/output/documentation"
+}
+
+detekt {
+    input = files("src/main/kotlin")
+    parallel = false
+    config = files("detekt/config.yml")
+    buildUponDefaultConfig = false
+    baseline = file("detekt/baseline.xml")
+    disableDefaultRuleSets = false
+    debug = false
+    ignoreFailures = false
+    reports {
+        xml {
+            enabled = true
+            destination = file("build/reports/detekt/detekt.xml")
+        }
+        html {
+            enabled = true
+            destination = file("build/reports/detekt/detekt.html")
+        }
+        txt {
+            enabled = true
+            destination = file("build/reports/detekt/detekt.txt")
+        }
+    }
 }
